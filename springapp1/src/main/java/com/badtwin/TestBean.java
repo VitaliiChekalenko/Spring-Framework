8bcdda79c88dd0b9260931d81e01a2b5f23a07af
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2019-09-03 15:31:56 +0300

    1st

--- springapp1/src/main/java/com/badtwin/TestBean.java
+++ springapp1/src/main/java/com/badtwin/TestBean.java
@@ -1,4 +1,18 @@
 package com.badtwin;
 
 public class TestBean {
+    private String name;
+
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- springapp1/src/main/java/com/badtwin/TestSpring.java
+++ springapp1/src/main/java/com/badtwin/TestSpring.java
@@ -1,4 +1,14 @@
 package com.badtwin;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "applicationContext.xml");
+    };
+
+    context.getBean();
+
+
 }
